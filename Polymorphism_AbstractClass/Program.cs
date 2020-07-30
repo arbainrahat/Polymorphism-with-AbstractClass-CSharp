691ed301fe9417a9c84b23a3b44d7f52d2f55e43
arbainrahat <68120270+arbainrahat@users.noreply.github.com>
2020-07-30 22:10:05 +0500

    Add files via upload

---
+++ Polymorphism_AbstractClass/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Polymorphism_AbstractClass.com.management;
+//using Polymorphism_AbstractClass.com.PurePolymorphism;
+using Polymorphism_AbstractClass.com.Explict_MultipleInhertance;
+namespace Polymorphism_AbstractClass
+{
+    class Program
+    {
+        
+       static void Main(string[] args)
+        {
+            /*  Transformer t = new Car();
+              t.show();
+              t.run();
+
+               t = new Boat();
+              t.run();
+
+               t = new Plane();
+              t.run();
+
+      */
+
+
+            //Interface use Here
+            /*
+            ITransformer ob = new Car();
+            ob.State();
+
+             ob = new Boat();
+            ob.State();
+
+             ob = new Plane();
+            ob.State();
+            */
+
+            //Use Multiple Inheretence, Explict Implimentation of method
+
+            Car c = new Car();
+            ITransformer it = c;
+            it.Run();
+
+            IExamle ie = c;
+            ie.Run();
+
+            Console.ReadLine();
+        }
+    }
+    
+}
